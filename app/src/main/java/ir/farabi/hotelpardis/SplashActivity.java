759af6676ff50562cf2bdc380b46b9e272da784a
ali <parsali@gmail.com>
2018-02-05 17:08:00 +0330

    No Message

---
+++ app/src/main/java/ir/farabi/hotelpardis/SplashActivity.java
@@ -0,0 +1,21 @@
+package ir.farabi.hotelpardis;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
