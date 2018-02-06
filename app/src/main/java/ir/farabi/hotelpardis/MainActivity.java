958b1eee683ddc64518cc6e95f6494b42667c79a
parsvaf <parsadino2@gmail.com>
2018-02-06 16:04:56 +0330

    infoUI

--- app/src/main/java/ir/farabi/hotelpardis/MainActivity.java
+++ app/src/main/java/ir/farabi/hotelpardis/MainActivity.java
@@ -1,20 +1,31 @@
 package ir.farabi.hotelpardis;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     databaseHandler DB;
+    ActionBar ab;
     private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        DB= new databaseHandler(getApplicationContext());
-        listView = (ListView)findViewById(R.id.list);
-        CustomListViewAdapter customListView = new CustomListViewAdapter(getApplicationContext());
-        listView.setAdapter(customListView);
+        setContentView(R.layout.more_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myBestoolbar);
+        setSupportActionBar(toolbar);
+        ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowHomeEnabled(true);
+
+//        DB= new databaseHandler(getApplicationContext());
+//        listView = (ListView)findViewById(R.id.list);
+//        CustomListViewAdapter customListView = new CustomListViewAdapter(getApplicationContext());
+//        listView.setAdapter(customListView);
 
     }
 }
---
+++ app/src/main/java/ir/farabi/hotelpardis/moreInfo.java
@@ -0,0 +1,14 @@
+package ir.farabi.hotelpardis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class moreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_info);
+
+    }
+}
